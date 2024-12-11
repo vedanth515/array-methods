ebb51f317bbf02f47354711a14530664e46d36d6
vedanth515 <vedanthkarnataka@gmail.com>
2024-12-11 21:00:32 +0530

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,95 @@
+// this are the first 10 questions
+
+// var array = ["vedanth","rahul","sai-kumar","sai-kiran","harsha"]
+
+// // console.log(array.length);
+
+// array.push("surya")
+
+// // console.log(array);
+
+// array.pop()
+
+// // console.log(array);
+
+// array.unshift("gagan")
+
+// // console.log(array);
+
+// array.shift()
+
+// // console.log(array);
+
+// array.reverse()
+
+// // console.log(array);
+
+// // console.log(array.length);
+
+// console.log(array.includes(["gagan","aravind","rohith","vedanth"]));
+
+// // console.log(array.join());
+
+
+
+
+
+
+/////////// this are the other 5 question ////////////
+
+// var array = ["vedanth","rahul","sai-kumar","sai-kiran","harsha"]
+
+
+// array.push("aravind")
+// console.log(array);
+
+
+// array.pop()
+// console.log(array);
+
+
+// console.log(array.reverse().join(array));
+
+
+// array.unshift("gagan","rahul2")
+// console.log(array);
+
+
+// var array = ["vedanth","rahul","sai-kumar","sai-kiran","harsha"]
+// var array1 = ["vedanth","rahul","sai-kumar","sai-kiran","harsha"]
+// console.log(array.concat(array1));
+
+
+// var z = "vedanth"
+// console.log(z.split(""));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
